92223678532b3fb0971a09dee6e93dcd59c01da7
Tom Chan <chanshingka1@gmail.com>
2021-04-04 14:38:07 +0800

    Add cors policy

--- src/main/java/com/example/artworkadmin/ArtworkAdminApplication.java
+++ src/main/java/com/example/artworkadmin/ArtworkAdminApplication.java
@@ -25,7 +25,7 @@ public class ArtworkAdminApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**").allowedMethods("*");
             }
         };
     }
